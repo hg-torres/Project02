347c0c8e6be7ebe635545b3ce79e5711cdb81e19
Chanwoo Hwang <Chanwoo1017@hotmail.com>
2022-01-03 10:41:42 -0800

    re

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,24 +1,13 @@
 const router = require('express').Router()
-<<<<<<< HEAD
 const { Anime , User} = require('../models')
-=======
-const { User, Post } = require('../models')
->>>>>>> 6f2e4751880393373888287a0210bcffd6e9b5b6
 const passport = require('passport')
 const jwt = require('jsonwebtoken')
 
 router.post('/users/register', (req, res) => {
-<<<<<<< HEAD
  User.register(new User({ username: req.body.username, email: req.body.email }), req.body.password, err => {
   if (err) { console.log(err) }
   res.sendStatus(200)
  })
-=======
-  User.register(new User({ firstName: req.body.firstName, lastName: req.body.lastName, username: req.body.username, email: req.body.email }), req.body.password, err => {
-    if (err) { console.log(err) }
-    res.sendStatus(200)
-  })
->>>>>>> 6f2e4751880393373888287a0210bcffd6e9b5b6
 })
 
 router.post('/users/login', (req, res) => {
