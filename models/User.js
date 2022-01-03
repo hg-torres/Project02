eddf2ede4217bb502bddbbf80d86da118e41535a
Chanwoo Hwang <Chanwoo1017@hotmail.com>
2022-01-03 10:41:31 -0800

    working on register

--- models/User.js
+++ models/User.js
@@ -3,7 +3,6 @@ const { Model, DataTypes } = require('sequelize')
 const sequelize = require('../db')
 
 const User = pls.defineUser(sequelize, {
-<<<<<<< HEAD
  firstName: {
   type: DataTypes.STRING,
   allowNull: false
@@ -16,20 +15,6 @@ const User = pls.defineUser(sequelize, {
   type: DataTypes.STRING,
   allowNull: true
  },
-=======
-  firstName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  lastName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  // profile: {
-  //   type: DataTypes.STRING,
-  //   allowNull: true
-  // },
->>>>>>> 6f2e4751880393373888287a0210bcffd6e9b5b6
   username: {
   type: DataTypes.STRING,
   allowNull: false
--- models/index.js
+++ models/index.js
@@ -5,13 +5,7 @@ const Friend = require('./Friend.js')
 
 User.hasMany(Anime, { foreignKey: 'uid' })
 Anime.belongsTo(User, { foreignKey: 'uid' })
-<<<<<<< HEAD
 User.hasMany(Friend, { foreignKey : 'uid'})
 // Friend.belongsToMany(User, {foreignkey : 'uid'})
-=======
-User.hasMany(Friend, { foreignKey: 'uid' })
-//Friend.belongsToMany(User, { foreignKey: 'uid' })
-
->>>>>>> 6f2e4751880393373888287a0210bcffd6e9b5b6
 
 module.exports = { User, Anime, Friend }
