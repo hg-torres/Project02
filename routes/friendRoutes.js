1a31ba302663cf57efe452eefa26b9770c1ae797
RAH <richardaustinhall@gmail.com>
2022-01-04 10:46:46 -0800

    cleaning

--- routes/friendRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const { Anime, User } = require('../models')
-const passport = require('passport')
-
-
-
-
-module.exports = router
--- routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-
-router.use('/api', require('./userRoutes.js'))
-router.use('/api', require('./animeRoutes.js'))
-
-module.exports = router
