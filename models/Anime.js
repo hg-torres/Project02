c1701b74dde7945876e152c7bbec425a8febe614
Chanwoo Hwang <Chanwoo1017@hotmail.com>
2022-01-04 13:57:22 -0800

    working

--- models/Anime.js
+++ models/Anime.js
@@ -3,25 +3,6 @@ const sequelize = require('../db')
 
 class Anime extends Model { }
 Anime.init({
-<<<<<<< HEAD
- title: {
-  type: DataTypes.STRING,
-  allowNull: false
- },
- image: {
-  type: DataTypes.STRING,
-  allowNull: false
- },
- rating: {
-  type: DataTypes.STRING,
-  allowNull: false
- },
- synopsis: {
-  type: DataTypes.STRING,
-  allowNull: false
- }
-}, { sequelize, modelName: 'Anime' })
-=======
   title: {
     type: DataTypes.STRING,
     allowNull: false
@@ -39,6 +20,5 @@ Anime.init({
     allowNull: false
   }
 }, { sequelize, modelName: 'anime' })
->>>>>>> f63a4e36a763e831c11fbe4d4073af87ced4f9d7
 
 module.exports = Anime
