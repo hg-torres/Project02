43207698615f67ceb95500ea4d759a89081970cc
Chanwoo Hwang <Chanwoo1017@hotmail.com>
2022-01-04 12:05:10 -0800

    testing

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const Friend = require('./Friend.js')
 
 User.hasMany(Anime, { foreignKey: 'uid' })
 Anime.belongsTo(User, { foreignKey: 'uid' })
-User.hasMany(Friend, { foreignKey : 'uid'})
+// User.hasMany(Friend, { foreignKey : 'uid'})
 // Friend.belongsToMany(User, {foreignkey : 'uid'})
 
 module.exports = { User, Anime, Friend }
