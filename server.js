82472e0051025b326246e655519a4c0bb7011693
Peter Song <psong562@gmnail.com>
2022-01-06 00:42:59 -0800

    update

--- server.js
+++ server.js
@@ -1,12 +1,59 @@
+// require('dotenv').config()
+
+// const express = require('express')
+// const { join } = require('path')
+
+// const passport = require('passport')
+// const { User, Anime } = require('./models') // change Post to actual var
+// const { Strategy: JWTStrategy, ExtractJwt } = require('passport-jwt')
+
+// const app = express()
+
+// app.use(express.static(join(__dirname, 'public')))
+// app.use(express.urlencoded({ extended: true }))
+// app.use(express.json())
+
+// app.use(passport.initialize())
+// app.use(passport.session())
+
+// passport.use(User.createStrategy())
+
+// passport.serializeUser(User.serializeUser())
+// passport.deserializeUser(User.deserializeUser())
+
+// passport.use(new JWTStrategy({
+//   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+//   secretOrKey: process.env.SECRET
+// }, async function ({ id }, cb) {
+//   try {                                         // change Post to actual var
+//     const user = await User.findOne({ where: { id }, include: [Anime] })
+//     cb(null, user)
+//   } catch (err) {
+//     cb(err, null)
+//   }
+// }))
+
+// app.use(require('./routes'))
+
+// async function init() {
+//   await require('./db').sync()
+//   app.listen(process.env.PORT || 3000)
+// }
+
+// init()
+
+// require('./db').sync()
+//   .then(() => app.listen(process.env.PORT || 3000))
+//   .catch(err => console.log(err))
+
+
 require('dotenv').config()
 
 const express = require('express')
 const { join } = require('path')
-
 const passport = require('passport')
-const { User, Anime } = require('./models') // change Post to actual var
+const { User } = require('./models')
 const { Strategy: JWTStrategy, ExtractJwt } = require('passport-jwt')
-
 const app = express()
 
 app.use(express.static(join(__dirname, 'public')))
@@ -17,30 +64,24 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 passport.use(User.createStrategy())
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
 
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+passport.deserializeUser((id, done) => {
+  User.findOne({ id })
+    .then(user => done(null, user))
+    .catch(err => done(err, null))
+})
 
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.SECRET
-}, async function ({ id }, cb) {
-  try {                                         // change Post to actual var
-    const user = await User.findOne({ where: { id }, include: [Anime] })
-    cb(null, user)
-  } catch (err) {
-    cb(err, null)
-  }
-}))
+}, ({ id }, cb) => User.findOne({ where: { id } })
+  .then(user => cb(null, user))
+  .catch(err => cb(err))))
 
 app.use(require('./routes'))
-
-// async function init() {
-//   await require('./db').sync()
-//   app.listen(process.env.PORT || 3000)
-// }
-
-// init()
 
 require('./db').sync()
   .then(() => app.listen(process.env.PORT || 3000))
